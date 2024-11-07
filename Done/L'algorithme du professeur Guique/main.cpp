022e5d0288c088faa8e1d1f97eca53f3ecb2264b
Alexandre Beaulieu <abeaulieu815@gmail.com>
2024-11-07 11:20:15 -0400

    fixed

--- Done/L'algorithme du professeur Guique/main.cpp
+++ Done/L'algorithme du professeur Guique/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main(){
     //initialiser a, b, c, k et n respectivement à 1, 4, 3, 1 et 0
-    double a = 2,b=4,c=3,k=1,n=0;
+    double a = 1,b=4,c=3,k=1,n=0;
 /*initialiser a, b, c, k et n respectivement à 1, 4, 3, 1 et 0
 répéter tant que k est strictement inférieur à 1000-n
     a ← b
@@ -13,10 +13,12 @@ répéter tant que k est strictement inférieur à 1000-n
     n ← a + b
     augmenter k de 1
 fin répéter*/
-for(int i = 0; i< 9;i++){
-    a = pow(a,2);
-    cout << a << endl;}
-    a = pow(a,1.5);
-    sqrt(a);
-    cout << a;
+    while(k< 1000-n){
+        a =b;
+        b = c+a;
+        c = (-4)*c - 3*a - b;
+        n = a+b;
+        k++;
+    }
+    cout << a <<',' << b << ',' <<c;
 }
